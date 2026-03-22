2e6fc7494f9c8e694d5ee058e3f22ae5874775a0
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-03-22 11:11:38 +0000

    Add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: ["image.tmdb.org"]
-    }
+  site: 'https://kinoslon.netlify.app',
+
+  image: {
+      domains: ["image.tmdb.org"]
+  },
+
+  integrations: [sitemap()]
 });
