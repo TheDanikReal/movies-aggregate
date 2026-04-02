96f77c790a724055b31d87d627eca4f58478bb7c
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-04-02 12:53:44 +0300

    Update lastmod date

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
 
   integrations: [
     sitemap({
-      lastmod: new Date("2026-03-25T12:36:43.939Z")
+      lastmod: new Date("2026-04-02T12:53:01.333Z")
     })
   ]
 });
